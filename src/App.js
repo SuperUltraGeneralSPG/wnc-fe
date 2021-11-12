f201b4e8c234c6496ba71eb43ba0d439e6367b6f
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-11-12 13:08:50 +0900

    initail commit + 기본 router 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Auth, Join, Profile, Board } from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/board" element={<Board />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
