3b370c780c4f49bf0f6389ea8ec64e593a34d947
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-11-13 00:23:02 +0900

    [Auth-01] login form 생성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,40 @@
+import React, { useState } from 'react';
 
 const Auth = () => {
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
+
+    // input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+
+    // login 버튼 클릭 이벤트
+    const onClickLogin = () => {
+        if (inputId === "0000" && inputPw === "0000") {
+            console.log("logged in!");
+        } else {
+            console.log("no way");
+        }
+    }
     return (
         <div>
-            Auth
+            <h2>Login</h2>
+            <div>
+                <label htmlFor='input_id'>ID : </label>
+                <input type='text' name='input_id' value={inputId} onChange={handleInputId} />
+            </div>
+            <div>
+                <label htmlFor='input_pw'>PW : </label>
+                <input type='password' name='input_pw' value={inputPw} onChange={handleInputPw} />
+            </div>
+            <div>
+                <button type='button' onClick={onClickLogin}>Login</button>
+            </div>
         </div>
     )
 }
