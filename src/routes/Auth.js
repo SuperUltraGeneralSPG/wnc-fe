9cec4801da95510eac169e953c73f811605b7830
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-11-13 00:55:17 +0900

    [Auth-02] navigate 기능 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
-    const [inputId, setInputId] = useState('')
-    const [inputPw, setInputPw] = useState('')
+    const navigate = useNavigate();
+    const [inputId, setInputId] = useState('');
+    const [inputPw, setInputPw] = useState('');
 
     // input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
     const handleInputId = (e) => {
-        setInputId(e.target.value)
+        setInputId(e.target.value);
     }
 
     const handleInputPw = (e) => {
-        setInputPw(e.target.value)
+        setInputPw(e.target.value);
     }
 
     // login 버튼 클릭 이벤트
     const onClickLogin = () => {
         if (inputId === "0000" && inputPw === "0000") {
-            console.log("logged in!");
+            navigate('/');
         } else {
-            console.log("no way");
+            alert('id 또는 password가 잘못되었습니다.');
         }
     }
+
+    const onClickJoin = () => {
+        navigate('/join');
+    }
+
     return (
         <div>
             <h2>Login</h2>
@@ -34,6 +41,7 @@ const Auth = () => {
             </div>
             <div>
                 <button type='button' onClick={onClickLogin}>Login</button>
+                <button type='button' onClick={onClickJoin}>Join</button>
             </div>
         </div>
     )
